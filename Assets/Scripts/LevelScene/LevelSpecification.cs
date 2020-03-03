21ff1fd798b55788a67f8847b91e6e915632c6fb
Juan Manuel Lambre <juanmlambre@gmail.com>
2020-03-03 18:23:25 -0300

    Add LevelSpecification

---
+++ Assets/Scripts/LevelScene/LevelSpecification.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[JsonConverter(typeof(JsonPathConverter))]
+public class LevelSpecification
+{
+
+    [JsonProperty("origin")]
+    public readonly LevelOrigin Origin;
+
+    [JsonProperty("minimal_dimension.rows")]
+    public readonly int Rows;
+
+    [JsonProperty("minimal_dimension.columns")]
+    public readonly int Columns;
+
+
+    public static LevelSpecification Load(string json)
+    {
+        return JsonConvert.DeserializeObject<LevelSpecification>(json);
+    }
+
+}
