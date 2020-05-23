1502e0791ed78a8a0b080bdb829a613fccfa9745
Martín Stancanelli <mastanca14@gmail.com>
2020-05-23 18:18:12 -0300

    Webview log message

--- Assets/Scripts/UI/WebViewContainerBehaviour.cs
+++ Assets/Scripts/UI/WebViewContainerBehaviour.cs
@@ -2,17 +2,25 @@
 
 public class WebViewContainerBehaviour : MonoBehaviour
 {
+    public UniWebView webView;
     private bool WebViewVisible = false;
 
     private void Start()
     {
+        UniWebView.SetWebContentsDebuggingEnabled(true);
+
+        webView.OnMessageReceived += (view, message) => {
+            Debug.Log("holis");
+            Debug.Log(message.RawMessage);
+        };
+
         UpdateContainerVisibility();
     }
 
     public void ToggleWebView()
     {
         WebViewVisible = !WebViewVisible;
-
+        
         UpdateContainerVisibility();
     }
 
