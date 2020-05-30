df927c881e78775775bb758e5ea11bbd2fb2df6e
Martín Stancanelli <mastanca14@gmail.com>
2020-05-30 19:02:18 -0300

    Create destination, pad and teleport classes for deserialization

---
+++ Assets/Scripts/Model/LevelDestination.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+
+[JsonConverter(typeof(JsonPathConverter))]
+public class LevelDestination
+{
+
+    [JsonProperty("position")]
+    public Coordinate Coordinate { get; set; }
+
+    [JsonProperty("orientation")]
+    public Orientation Orientation { get; set; }
+
+
+    public static LevelDestination Load(string json)
+    {
+        return JsonConvert.DeserializeObject<LevelDestination>(json);
+    }
+
+}
