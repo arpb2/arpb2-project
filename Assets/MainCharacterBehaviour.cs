d2dee13b2bfb9df24ef8a4f70959369f547cb6fa
Juan Manuel Lambre <juanmlambre@gmail.com>
2020-06-06 20:06:48 -0300

    Add ARPB2 asset

--- Assets/MainCharacterBehaviour.cs
+++ Assets/MainCharacterBehaviour.cs
@@ -2,11 +2,14 @@
 
 public class MainCharacterBehaviour : ElementBehaviour
 {
+    public float RotationSpeed = 40f;
     public Orientation Orientation;
 
     public bool ExecutingAction { private set; get; }
 
     private Animator animator;
+    private float previousAngle;
+    private float limitRotation;
 
 
     public void Start()
@@ -14,24 +17,53 @@ public class MainCharacterBehaviour : ElementBehaviour
         animator = GetComponent<Animator>();
     }
 
+    public void Update()
+    {
+        if (animator.GetBool("RotateRight_Anim"))
+        {
+            var delta = RotationSpeed * Time.fixedDeltaTime;
+
+            Vector3 newAngle = transform.eulerAngles;
+            newAngle[1] = Mathf.Min(newAngle[1] + delta, previousAngle + 90f);
+
+            transform.eulerAngles = newAngle;
+
+            if (newAngle[1] >= previousAngle + 90f) OnTurnRightFinished();
+        }
+        else if (animator.GetBool("RotateLeft_Anim"))
+        {
+            Debug.Log(">>> rotating left...");
+            var delta = RotationSpeed * Time.fixedDeltaTime;
+
+            Vector3 newAngle = transform.eulerAngles;
+            newAngle[1] -= delta;
+            if (newAngle[1] < 0) newAngle[1] += 360f;
+            newAngle[1] = Mathf.Max(newAngle[1], limitRotation);
+
+            transform.eulerAngles = newAngle;
+
+            if (transform.eulerAngles[1] <= limitRotation) OnTurnLeftFinished();
+        }
+
+    }
+
     public void MoveForward()
     {
-        Debug.Log(">>> ARPB2 moves forward");
         ExecutingAction = true;
-        animator.SetBool("IsWalking", true);
+        animator.SetBool("Walk_Anim", true);
     }
 
     public void OnMoveForwardFinished()
     {
-        Debug.Log(">>> ARPB2 finished moving forward");
         ExecutingAction = false;
-        animator.SetBool("IsWalking", false);
+        animator.SetBool("Walk_Anim", false);
     }
 
     public void TurnRight()
     {
         ExecutingAction = true;
-        animator.SetBool("IsRotatingRight", true);
+        previousAngle = transform.eulerAngles[1];
+        animator.SetBool("RotateRight_Anim", true);
 
         Orientation newOrientation = Orientation.Equals(Orientation.N) ? Orientation.E :
                 Orientation.Equals(Orientation.E) ? Orientation.S :
@@ -44,13 +76,15 @@ public class MainCharacterBehaviour : ElementBehaviour
     public void OnTurnRightFinished()
     {
         ExecutingAction = false;
-        animator.SetBool("IsRotatingRight", false);
+        animator.SetBool("RotateRight_Anim", false);
     }
 
     public void TurnLeft()
     {
         ExecutingAction = true;
-        animator.SetBool("IsRotatingLeft", true);
+        limitRotation = transform.eulerAngles[1] - 90f;
+        if (limitRotation < 0) limitRotation += 360f;
+        animator.SetBool("RotateLeft_Anim", true);
 
         Orientation newOrientation = Orientation.Equals(Orientation.N) ? Orientation.W :
                 Orientation.Equals(Orientation.W) ? Orientation.S :
@@ -63,7 +97,7 @@ public class MainCharacterBehaviour : ElementBehaviour
     public void OnTurnLeftFinished()
     {
         ExecutingAction = false;
-        animator.SetBool("IsRotatingLeft", false);
+        animator.SetBool("RotateLeft_Anim", false);
     }
 
 
